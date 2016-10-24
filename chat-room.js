de5489f002a371163990ef076a94e03425bd4f86
Tyler Smith <tyler.smith6891@gmail.com>
2016-10-24 16:12:29 -0700

    random names generated

--- chat-room.js
+++ chat-room.js
@@ -2,14 +2,26 @@
 
 // this module should handle adding/removing clients, as well as sending messages
 
+
 module.exports = class ChatRoom {
+
     constructor() {
+        this.users = ['sonic', 'mario', 'kirby', 'pacman', 'charizard'];
+        this.nameCounter = 1;
         this.clientCounter = 1;
         this.clients = [];
+        this.currentUsers = [];
     }
 
     add(client) {
-        client.name = `client ${this.clientCounter++}`;
+        const randomName = this.users[Math.floor(Math.random() * this.users.length)];
+        if (this.currentUsers.indexOf(randomName) !== -1) {
+            client.name = `${randomName} ${this.nameCounter}`;
+            this.nameCounter++;
+        } else {
+            client.name = randomName;
+        };
+        this.currentUsers.push(randomName);
         this.clients.push(client);
         console.log(`${client.name} has connected!`);
     }
