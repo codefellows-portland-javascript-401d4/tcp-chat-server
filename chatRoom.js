4c71536c1cbe1a35f6126ac75526f5175197b7d9
Andrew Beers <andybeers@gmail.com>
2016-10-24 14:03:03 -0700

    Completed ChatRoom  class with prototype methods. exported.

--- chatRoom.js
+++ chatRoom.js
@@ -1,21 +1,45 @@
 const generate = require('project-name-generator');
+const net = require('net');
 
 module.exports = class ChatRoom {
 
   constructor() {
-    this.name
+    this.nick = generate({alliterative: true}).dashed;
+    this.clients = [];
   }
 
-  add() {
-
+  add(client) {
+    client.nick = this.nick;
+    this.clients.push(client);
   }
 
-  send() {
-
+  send(sender, message) {
+    this.clients.forEach(c => {
+      if(c === sender) return;
+      c.write(`${sender.nick}: ${message}`);
+    });
   }
 
-  remove() {
+  remove(client) {
+    const index = this.clients.indexOf(client);
+    if (index !== -1) this.clients.splice(index, 1);
+  }
 
+  changeNick(client, message) {
+    let oldNick = client.nick;
+    let newNick = message.split(' ')[1];
+    client.nick = newNick;
+    this.clients.forEach(c => {
+      c.write(`${oldNick} has changed their name to ${client.nick}`);
+    });
+  }
+
+  changeNickRandom(client) {
+    let oldNick = client.nick;
+    client.nick = this.nick;
+    this.clients.forEach(c => {
+      c.write(`${oldNick} has changed their name to ${client.nick}`);
+    });
   }
 
 };
