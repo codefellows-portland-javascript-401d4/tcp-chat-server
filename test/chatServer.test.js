cd9b5991637a1360ac79d0c5a3f8bf2da94d2285
Michelle Sri <michelle.sri@gmail.com>
2016-10-24 16:53:54 -0700

    working version with all tests passing

--- test/chatServer.test.js
+++ test/chatServer.test.js
@@ -1,12 +1,33 @@
-const net = require('net');
-const server = require('../lib/server');
 const assert = require('chai').assert;
+const ChatRoom = require('../ChatRoom');
 
-const port = 65000;
+describe('chat room', () => {
 
-describe('server', () => {
-  it('check if server started', (done) => {
-    server.runTelnetServer();
-    done();
+  const chatRoom = new ChatRoom();
+
+  class DummyClient {
+    write(message){
+      this.received = message;
+    }
+  }
+
+  const dummyClient1 = new DummyClient();
+  const dummyClient2 = new DummyClient();
+
+  it('clients can join room', () => {
+    assert.equal(chatRoom.clients.length, 0);
+    chatRoom.add(dummyClient1);
+    assert.equal(chatRoom.clients.length, 1);
+    assert.equal(chatRoom.clients[0], dummyClient1);
+    assert.equal(dummyClient1.name, 'guest 1');
+  });
+
+  it('broadcasts messages to all clients in room', () => {
+    chatRoom.add(dummyClient2);
+    chatRoom.chat(dummyClient2, 'hi');
+    assert.equal(dummyClient1.received, 'guest 2: hi');
+    //assert.Ok passes when the value inside is truthy
+    //assert.isNotOk passes when the value inside is falsey;
+    assert.notEqual(dummyClient2.received, 'guest 2: hi');
   });
 });
