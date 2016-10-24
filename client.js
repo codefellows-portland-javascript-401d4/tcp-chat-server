f117833b16fe5665bc5d07d51e929718d0f1374b
Mugsy Carter <mugsycarter@gmail.com>
2016-10-24 13:15:58 -0700

    First Commit, basic skeleton of project added.

---
+++ client.js
@@ -0,0 +1,16 @@
+class ChatRoom {
+    constructor(){
+        this.seed = 1;
+        this.clients = [];
+}
+
+add(client){
+    client.name ='client ' + (this.seed++);
+    this.clients.push(client);
+}
+
+send(sender,message)
+
+ChatRoom.prototype.add = function(){
+
+};
