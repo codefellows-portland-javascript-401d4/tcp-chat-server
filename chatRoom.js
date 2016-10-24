a998f0698db0502b1f9764c9c7982e894f6bf89c
Andrew Beers <andybeers@gmail.com>
2016-10-24 13:24:15 -0700

    Set up project basics (gitignore, package, travis etc)

---
+++ chatRoom.js
@@ -0,0 +1,21 @@
+const generate = require('project-name-generator');
+
+module.exports = class ChatRoom {
+
+  constructor() {
+    this.name
+  }
+
+  add() {
+
+  }
+
+  send() {
+
+  }
+
+  remove() {
+
+  }
+
+};
---
+++ tcpChat.js
@@ -0,0 +1,8 @@
+const net = require('net');
+
+const server = net.createServer(client => {
+  client.setEncoding('utf-8');
+
+
+
+});
