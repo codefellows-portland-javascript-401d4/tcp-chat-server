e1bcf6bc4c521eff7575f5f434756b407face621
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-10-24 14:58:01 -0700

    Working chat room with name changing feature using /nick <name>

---
+++ tcp-chat-server.js
@@ -0,0 +1,35 @@
+const net = require('net');
+const ChatRoom = require('./ChatRoom');
+
+const chatRoom = new ChatRoom();
+
+const server = net.createServer(client => {
+  client.setEncoding('utf-8');
+  chatRoom.addClient(client);
+  console.log(`${client.name} joined the chat...`);
+  
+  client.on('data', message => {
+    let re = /\/nick\s+/;
+    if (re.test(message)) {
+      let newName = message.replace(re, '').trim();
+      let bcastNameChange = `${client.name} changing name to ${newName}`
+      chatRoom.broadcast(bcastNameChange);
+      console.log(bcastNameChange);
+      client.name = newName;
+    }
+    else {
+      // send this to all the other clients
+      chatRoom.send(client, message);
+    }
+  });
+
+  client.on('close', () => {
+    console.log(`${client.name} left the chat...`);
+    chatRoom.removeClient(client);
+  });
+});
+
+const port = 65000;
+server.listen(port, err => {
+  console.log('Server listening on port', port, err);
+});
