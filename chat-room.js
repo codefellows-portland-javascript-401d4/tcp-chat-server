bb3d0aba1a91122c43ec44892c1622de1759925b
Tyler Smith <tyler.smith6891@gmail.com>
2016-10-25 13:41:10 -0700

    minor refactoring

--- chat-room.js
+++ chat-room.js
@@ -6,7 +6,7 @@
 module.exports = class ChatRoom {
 
     constructor() {
-        this.users = ['sonic', 'mario', 'kirby', 'pacman', 'charizard'];
+        this.users = ['sonic', 'mario', 'kirby', 'pacman', 'charizard', 'link', 'samus'];
         this.nameCounter = 1;
         this.clientCounter = 1;
         this.clients = [];
@@ -16,20 +16,25 @@ module.exports = class ChatRoom {
     add(client) {
         const randomName = this.users[Math.floor(Math.random() * this.users.length)];
         if (this.currentUsers.indexOf(randomName) !== -1) {
-            client.name = `${randomName} ${this.nameCounter}`;
-            this.nameCounter++;
+            client.name = `${randomName}(${this.nameCounter++})`;
         } else {
             client.name = randomName;
         };
-        this.currentUsers.push(randomName);
+        this.currentUsers.push(client.name);
         this.clients.push(client);
         console.log(`${client.name} has connected!`);
+        console.log(this.currentUsers);
     }
 
     remove(client) {
         const index = this.clients.indexOf(client);
-        if (index !== -1) this.clients.splice(index, 1);
+        if (index !== -1) {
+            this.clients.splice(index, 1);
+            this.currentUsers.splice(index, 1);
+        }
         console.log(`${client.name} has disconnected!`);
+        console.log(this.currentUsers);
+
     }
 
     send(client, msg) {
