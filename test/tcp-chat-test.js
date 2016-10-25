a362f1ae1c5bf1ab02c2e821af676a17e341dc66
caraldi <araldicm@Caitlin-M-Araldi.local>
2016-10-25 14:39:10 -0700

    Modified path to module in tcp-chat.js, wrote initial tcp-chat-test

---
+++ test/tcp-chat-test.js
@@ -0,0 +1,30 @@
+const assert = require('chai').assert;
+const chatRoom = require('./dog-chat-module');
+
+describe('chat room', () => {
+    const dogChat = new DogChat();
+
+    class MockClient {
+        write(message) {
+            this.received = message;
+        }
+    }
+
+    const dogClient1 = new MockClient();
+    const dogClient2 = new MockClient();
+
+    it('adds clients', () => {
+        assert.equal(dogChat.dogClients.length, 0);
+        dogChat.add(dogClient1);
+        assert.equal(dogChat.dogClients.length, 1);
+        assert.equal(dogChat.dogClients[0], dogClient1);
+        assert.equal(dogClient1.username, 'dog 1');
+    });
+
+    it('broadcasts sent messages to other client', () => {
+        dogChat.add(dogClient2);
+        dogChat.send(dogClient2, 'ruff');
+        assert.equal(dogClient1.received, 'dog 2: ruff');
+        assert.isNotOk(dogClient2.received);
+    });
+});
