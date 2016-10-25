28e20a4135d1905b39f6d7c1d07552fc5ebc79d5
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-10-25 15:47:57 -0700

    Added E2E testing with client sockets in test JS; started ChatClient.js to work on CLI client

--- test/tcp-chat-server.test.js
+++ test/tcp-chat-server.test.js
@@ -1,5 +1,8 @@
 const assert = require('chai').assert;
 const ChatRoom = require('../lib/ChatRoom');
+// const ChatClient = require('../lib/ChatClient');
+const server = require('../lib/ChatServer');
+const net = require('net');
 
 describe('ChatRoom unit tests', () => {
 
@@ -34,10 +37,84 @@ describe('ChatRoom unit tests', () => {
 
 });
 
-describe ('ChatRoom server testing', () => {
+describe ('ChatRoom end-to-end', () => {
+  const port = 65001;
 
-  it ('does something', () => {
+  before (done => {
+    server.listen(port, done);
+  });
 
+  describe ('ChatServer', () => {
+
+    let client1 = null;
+    let client2 = null;
+
+    before (done => {
+      client1 = net.connect({port: port}, (err) => {
+        if (err) done(err);
+        else {
+          client1.setEncoding('utf-8');
+          done();
+        }
+      });
+    });
+
+    it ('replies when a client joins the chat', (done) => {
+      client1.once('data', data => {
+        // console.log('client1 received ', data);
+        assert.equal(data, 'Broadcast: Client 1 joined the chat...\n');
+        done();
+      });
+      // let client1 = new ChatClient();
+      // client1.connect('localhost', 65001, (err) => {
+      //   if (err) done(err);
+      //   assert.equal(client1.received === `Broadcast: ${client1.name} joined the chat...`);
+      // });
+    });
+
+    it ('replies and broadcasts when a 2nd client joins the chat', (done) => {
+      client1.once('data', data => {
+        assert.equal(data, 'Broadcast: Client 2 joined the chat...\n');
+      });
+      client2 = net.connect({port: port}, (err) => {
+        if (err) done(err);
+        else {
+          client2.setEncoding('utf-8');
+          client2.once('data', data => {
+            assert.equal(data, 'Broadcast: Client 2 joined the chat...\n');
+            done();
+          });
+        }
+      });
+    });
+
+    it ('sends messages to all clients except the sender', (done) => {
+      client2.once('data', data => {
+        assert.equal(data, 'Client 1: Hi, Client 2!\n');
+        done();
+      });
+      client1.write('Hi, Client 2!\n');
+    });
+
+    it ('broadcasts message when a client leaves the chat', (done) => {
+      client1.once('data', (data) => {
+        assert.equal(data, 'Broadcast: Client 2 left the chat...\n');
+        done();
+      });
+      client2.once('close', (err) => {
+        if (err) done(err);
+      });
+      client2.end();
+    });
+
+    after (done => {
+      client1.end(done);
+    });
+
+  });
+
+  after (done => {
+    server.close(done);
   });
 
 });
