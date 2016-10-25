a56d0e934c7a48f83b5a307bbf6c47d6828d6862
Mugsy Carter <mugsycarter@gmail.com>
2016-10-25 14:45:40 -0700

    chat program now working as intended as well as the rename function.

--- chatRoom.js
+++ chatRoom.js
@@ -1,5 +1,3 @@
-var nicknames = require('nicknames');
-
 module.exports = class ChatRoom {
     constructor() {
         this.seed = 1;
@@ -9,66 +7,34 @@ module.exports = class ChatRoom {
     add(client) {
         client.name = 'client ' + (this.seed++);
         this.clients.push(client);
-        client.write('Welcome to the chatroom, ' + client.name + '.  If you do not like your username you can change it to a nickname with the following commands: \n "/lady" = change to a female nickname. \n "/gent" = change to a male nickname.  \n "/whocares" = change to any nickname. \n');
+        client.write('Welcome to the chatroom, ' + client.name + '.  If you do not like your username you can change it to a nickname with the following commands: \n "/lady" = change to a female nickname. \n "/gent" = change to a male nickname.  \n "/whocares" = change to any nickname. \n\n');
     }
 
     send(sender, message) {
-        //send this to all the other clients
         this.clients.forEach(c => {
-            // if same as sender, return (exit this particular forEach invocation)
             if(c === sender) return;
-            // send the message
             c.write(`${sender.name}: ${message}`);
         });
     }
 
-    rename(client) {
-        console.log(this.clients);
+    rename(client, newName) {
         const oldName = client.name;
         const index = this.clients.indexOf(client);
         if (index !== -1) this.clients.splice(index, 1);
-        //TODO still working on this part
-        client.name = 'steve';
+        client.name = newName;
         this.clients.push(client);
         this.clients.forEach(c => {
-            c.write(oldName + ' is now ' + client.name + '.');
+            c.write(oldName + ' is now ' + client.name + '. \n');
         });
-        console.log(this.clients);
     }
 
     remove(client) {
         // remove from array:
+        this.clients.forEach(c => {
+            c.write(client.name + ' has left the chatroom.\n');
+        });
         const index = this.clients.indexOf(client);
         if (index !== -1) this.clients.splice(index, 1);
     }
 };
 
-
-// module.exports = class ChatRoom {
-    // constructor() {
-    //     this.clients = []
-    // }
-
-    // add(client) {
-    //     var nick = nicknames.allRandom();
-    //     while (this.clients.indexOf(nick)!==-1){
-    //        nick = nicknames.allRandom();
-    //     }
-    //     this.clients.push(client);
-    // }
-
-    // send(sender, message) {
-    //     //send this to all the other clients
-    //     this.clients.forEach(c => {
-    //         // if same as sender, return (exit this particular forEach invocation)
-    //         if(c === sender) return;
-    //         // send the message
-    //         c.write(`${sender.nick}: ${message}`);
-    //     });
-    // }
-
-    // remove(client) {
-    //     // remove from array:
-    //     const index = this.clients.indexOf(client);
-    //     if (index !== -1) this.clients.splice(index, 1);
-    // }
--- index.js
+++ index.js
@@ -1,86 +1,10 @@
 const net = require('net');
-// const nicknames = require('nicknames');
-const ChatRoom = require('./ChatRoom');
+var server = require('./server');
 
-const chatRoom = new ChatRoom();
-
-const server = net.createServer(client => {
-    client.setEncoding('utf-8');
-
-    chatRoom.add(client);
-
-    client.on('data', message => {
-        var ladypatt = /^\/lady/g;
-        var gentpatt = /^\/gent/g;
-        var patt = /^\/whocares/g;
-        if (ladypatt.test(message)){
-            chatRoom.rename(client);
-        }
-        else if (gentpatt.test(message)){
-            chatRoom.rename(client);
-        }
-        else if (patt.test(message)){
-            chatRoom.rename(client);
-        }
-        else{
-            chatRoom.send(client, message);
-        }
-    });
-
-    client.on('close', () => {
-        chatRoom.remove(client);
-    });
-});
 
 const port = 65000;
 server.listen(port, err => {
     if (err) console.log('ERROR!', err);
     else console.log('server listening on port', port);
-})
-
-
-// const port = 65000;
-// var client = null;
-
-// server.listen(port, function(){
-//     client = net.connect({port : port}), err=>{
-//         if (err) console.log(err);
-//         else
-//             client.setEncoding = 'utf-8';
-//     };
-// });
-
-
-
-// const clients = [];
-
-// const server = net.createServer(client => {
-//     const name =  nicknames.allRandom();
-//     client.setEncoding('utf-8');
-
-//     clients.push(client);
-
-//     client.on('data', message => {
-//         //send this to all the other clients
-//         clients.forEach(c => {
-//             // if same as sender, return (exit this particular forEach invocation)
-//             if(c === client) return;
-//             // otherwise send the message
-//             c.write(`${name}: ${message}`);
-//         });
-//     });
-
-//     client.on('close', () => {
-//         // remove from array:
-//         const index = clients.indexOf(client);
-//         if (index !== -1) clients.splice(index, 1);
-//         console.log(`client ${name} has disconnected`);
-//     });
-// });
-
-// const port = 65000;
-// server.listen(port, err => {
-//     if (err) console.log('ERROR!', err);
-//     else console.log('server listening on port', port);
-// });
+});
 
