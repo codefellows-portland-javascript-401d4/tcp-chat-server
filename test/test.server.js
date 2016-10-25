15dc14b951f1b81645499e85d23b378953e21433
Gloria Guy <totallygloria@gmail.com>
2016-10-25 14:59:43 -0700

    Added a second test which ensures when 2 clients connect to the server, both are added to the chat array. server now exports the chat module as chat, test import chat, to make the connection.

--- test/test.server.js
+++ test/test.server.js
@@ -1,21 +1,33 @@
 const assert = require('chai').assert;
 const net = require('net');
-const server = require('../server');
-const chatModule = require('../chat-module');
+const server = require('../server').server;
+const chat = require('../server').chat;
 
 describe('chat-module', () => {
 
   const port = 65000;
+  let client = null;
 
   before(done => {
-    server.listen(port, done);
+    server.listen(port, () => {
+      client = net.connect({port: port});
+      client.setEncoding = 'utf-8';
+      client2 = net.connect({port: port});
+      client2.setEncoding = 'utf-8';
+      done();
+    });
   });
 
-  it('does something when a client connects', done => {
-    
-    
-    
+  it('says Hello when a client connects', done => {
+    client.once('data', data => {
+      assert.equal(data, 'Hello');
+      done();
+    });
   });
-  
+
+  it('adds a user to the users array', done => {
+    assert.equal(chat.clients.length, 2);
+    done();
+  });
 
 });
