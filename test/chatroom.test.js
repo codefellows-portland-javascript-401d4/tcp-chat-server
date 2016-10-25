b660a2ef6a725e178f450713ab0f62470087ec4c
Nathan Pickard <nathan.pickard87@gmail.com>
2016-10-25 16:00:08 -0700

    Added working testing file

--- test/chatroom.test.js
+++ test/chatroom.test.js
@@ -12,4 +12,20 @@ describe('Testing chat room', () => {
 
     const client1 = new MockClient();
     const client2 = new MockClient();
-})
+
+    it('Adds new clients', () => {
+        assert.equal(chatRoom.clients.length, 0);
+        chatRoom.add(client1);
+        // const client = {};
+        assert.equal(chatRoom.clients.length, 1);
+        assert.equal(chatRoom.clients[0], client1);
+        assert.equal(client1.name, 'Client 1');
+    });
+
+    it('Broadcast sends to other client', () => {
+        chatRoom.add(client2);
+        chatRoom.send(client2, 'hello');
+        assert.equal(client1.received, 'Client 2: hello');
+        assert.isNotOk(client2.received);
+    });
+});
