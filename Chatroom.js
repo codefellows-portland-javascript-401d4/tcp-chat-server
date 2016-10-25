e37f0dae43e5c986ab84059b3d42b6e2e21056da
Christopher Bruner <chrbruner9@gmail.com>
2016-10-24 17:05:19 -0700

    created server and added initial test, put Chatroom into it's own module

---
+++ Chatroom.js
@@ -0,0 +1,32 @@
+module.exports = class Chatroom {
+    constructor() {
+        this.seed = 1;
+        this.clients = [];
+    }
+    add(client) {
+        client.name = 'client ' + (this.seed++);
+        this.clients.push(client);
+    }
+    send(sender, message) {
+        this.clients.forEach(c => {
+            if(c === sender) return;
+            c.write(`${sender.name}: ${message}`);
+        });
+    }
+    remove(client) {
+        const index = this.clients.indexOf(client);
+        if (index !== -1) this.clients.splice(index, 1);
+    }
+    whoshere() {
+        this.clients.forEach(c => {
+            c.write(`${c.name}` + '\n');
+        });
+    }
+    changeName(client, newName) {
+        var oldName = client.name;
+        client.name = newName;
+        this.clients.forEach(c => {
+            c.write(`${oldName} changed name to ${client.name}` + '\n');
+        });
+    }
+};
