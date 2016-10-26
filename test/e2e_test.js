2ef0c80fa8e30df44ed91b1b30297a73dec63760
DrewStock <andrewdstock@gmail.com>
2016-10-25 17:44:21 -0700

    finished e2e_test.js ...for now

--- test/e2e_test.js
+++ test/e2e_test.js
@@ -2,7 +2,9 @@ const net = require('net');
 const assert = require('chai').assert;
 const server = require('../index');
 // const ChatGroup = require('../lib/chatGroup');
+// const chatGroup = new ChatGroup;
 const port = 3000;
+
 
 describe('server', () => {
 
@@ -26,22 +28,21 @@ describe('base functionality', () => {
     it('server welcomes client when they connect', done => {
         client.once('data', data => {
             console.log('server message to client:', data.toString());
-            assert.equal(data, 'welcome new client');
+            assert.equal(data, 'welcome new client\n');
             done();
         });
     });
 
+
+    // it('clients broadcast messages to all clients', done => {
+    //     client.once('data', message => {
+    //         chatGroup.send(client, message);
+    //         done();
+    //     });
+    // });
+
     after(done => {
         client.end(done);
     });
-
-
-
-
-
-
-
-
-
 
 });
--- test/unit_test.js
+++ test/unit_test.js
@@ -1,36 +1,36 @@
-// const assert = require('chai').assert;
-//
-// const ChatGroup = require('../lib/chatGroup');
-//
-// describe('chat', () => {
-//
-//     const chatGroup = new ChatGroup();
-//
-//     class testClient {
-//         write(received) {
-//             this.received = received;
-//         }
-//     }
-//
-//     const clientA = new testClient();
-//     const clientB = new testClient();
-//
-//     it('creates a new client', () => {
-//         assert.equal(chatGroup.clients.length, 0);
-//         chatGroup.create(clientA);
-//         assert.equal(chatGroup.clients.length, 1);
-//         assert.equal(chatGroup.clients[0], clientA);
-//     });
-//
-//     it('sends message to all clients', () => {
-//         chatGroup.create(clientB);
-//         chatGroup.send(clientB, 'hello');
-//         assert.isNotOk(clientB.received);
-//         assert.isOk(clientA.received);
-//         assert.equal(clientA.received, `${clientB.name}: hello`);
-//     });
-//
-//
-//
-//
-// });
+const assert = require('chai').assert;
+
+const ChatGroup = require('../lib/chatGroup');
+
+describe('Unit test - chat group messages', () => {
+
+    const chatGroup = new ChatGroup();
+
+    class testClient {
+        write(received) {
+            this.received = received;
+        }
+    }
+
+    const clientA = new testClient();
+    const clientB = new testClient();
+
+    it('creates a new client', () => {
+        assert.equal(chatGroup.clients.length, 0);
+        chatGroup.create(clientA);
+        assert.equal(chatGroup.clients.length, 1);
+        assert.equal(chatGroup.clients[0], clientA);
+    });
+
+    it('sends message to all clients', () => {
+        chatGroup.create(clientB);
+        chatGroup.send(clientB, 'hello');
+        assert.isNotOk(clientB.received);
+        assert.isOk(clientA.received);
+        assert.equal(clientA.received, `${clientB.name}: hello`);
+    });
+
+
+
+
+});
