6549f9720f9d3939b177721129704ec451f540f6
Mugsy Carter <mugsycarter@gmail.com>
2016-10-25 17:06:51 -0700

    tests fleshed out.  e2e test still in process.

--- test/betterTest.js
+++ test/betterTest.js
@@ -1,48 +1,77 @@
 const net = require('net');
 const assert = require('chai').assert;
-const chai = require('chai')
-const chaiHttp = require('chai-http');
 const server = require('../server');
+const ChatRoom = require('../chatRoom');
 
+const chatRoom = new ChatRoom();
 const port = 65000;
 var client = null;
+var client2 = null;
 
-describe('chat program', function(done){
-    before(done, function(){
-        server.listen(port, function(){
-            client = net.connect({port : port}), err=>{
-                if (err) done (err);
-                else
-                    client.setEncoding = 'utf-8';
-                done();
-            };
+describe('chat program', function(done){     
+    before(done => {
+        server.listen(port, done);
+    });
+
+    describe('basic client functionality', () => {
+        before(done => {
+            client = net.connect({ port: port }, err => {
+                if (err) done(err);
+                else {
+                    console.log('server connected on port', port);
+                    client.setEncoding('utf-8');
+                    done();
+                }
+            });
         });
+
+        before(done => {
+            client2 = net.connect({ port: port }, err => {
+                if (err) done(err);
+                else {
+                    client2.setEncoding('utf-8');
+                    done();
+                }
+            });
+        });
+
+
+
+        before(done => {
+            chatRoom.add(client);
+            chatRoom.add(client2);
+            done();
+        });
+
+        it('allows clients to message on another', function(done){
+            client2.write('Test hello');
+            client.on('data', function(data){
+                console.log('data are ', data);
+                assert('Test hello' === data);
+                chatRoom.remove(client); 
+                chatRoom.remove(client2);
+                console.log('semifinal clients are ', chatRoom.clients.length);
+                done();
+            });
+            client2.write('Test hello');
+        });
+    
+        // it('removes clients', function(done){
+        //     console.log('final clients are ', chatRoom.clients.length);
+        //     assert(0 === chatRoom.clients.length);
+        //     done();
+        // });
+
+    
+        // after(done => {
+        //     client.once('close', done);
+        //     client.end(done);
+        // });  
+
+        // after(done => {
+        //     server.close(done);
+        // });
+
     });
-
-    it('creates clients', function(done){
-        console.log('in first test');
-        console.log('client = ', client);
-        assert(client != null);
-        done();
-    });
-
-     it('assigns client names', function(done){
-        console.log('in second test');
-    //     console.log('client name = ', client);
-    //     assert(client != null);
-    //     done();
-    // });
-
-
-
-
-    after(done => {
-        client.once('close', done);
-        client.end(done);
-    });  
-
-    after(done => {
-        server.close(done);
-    });
-
+     done;
 });
--- test/test.js
+++ test/test.js
@@ -1,29 +1,100 @@
 const net = require('net');
-const nicknames = require('nicknames');
-const assert = require('assert');
+const assert = require('chai').assert;
+const server = require('../server');
+const ChatRoom = require('../chatRoom');
 
+const chatRoom = new ChatRoom();
+const port = 65000;
+var client = null;
+var client2 = null;
 
-describe('chat program', function(done){
-    it('creates clients', function(done){
-    const clients = [];
-
-    const server = net.createServer(client => {
-        const name =  nicknames.allRandom();
-        client.setEncoding('utf-8');
-        clients.push(client);
-
-    const port = 65000;
-    server.listen(port, err => {
-        if (err) console.log('ERROR!', err);
-        else console.log('server listening on port', port);
+describe('chat program', function(done){     
+    before(done => {
+        server.listen(port, done);
     });
-      
 
-        done();
-        console.log('in first test');
-        console.log('clients length', clients.length);
-        assert(clients.length === 1);
-        done();
+    describe('basic client functionality', () => {
+        before(done => {
+            client = net.connect({ port: port }, err => {
+                if (err) done(err);
+                else {
+                    console.log('server connected on port', port);
+                    client.setEncoding('utf-8');
+                    done();
+                }
+            });
         });
+
+        before(done => {
+            client2 = net.connect({ port: port }, err => {
+                if (err) done(err);
+                else {
+                    client2.setEncoding('utf-8');
+                    done();
+                }
+            });
+        });
+
+
+
+        before(done => {
+            chatRoom.add(client);
+            chatRoom.add(client2);
+            done();
+        });
+
+        it('creates multiple clients', function(done){
+            console.log('clients length is ', chatRoom.clients.length);
+            assert(chatRoom.clients.length === 2);
+            done();
+        });
+        
+        it('names clients', function(done){
+            assert(client.name === 'client 1');
+            chatRoom.rename(client, 'Steve');
+            done();
+        });
+
+        it('greets clients with a message', function(done){
+            assert('Welcome to the chatroom, client 1.  If you do not like your username you can change it to a nickname with the following commands: \n "/lady" = change to a female nickname. \n "/gent" = change to a male nickname.  \n "/whocares" = change to any nickname. \n\n' === client.greeting);
+            done();
+        });
+
+        it('changes client names', function(done){
+            assert(client.name === 'Steve');
+            chatRoom.send(client, 'fart');
+            console.log('farted');
+            done();
+        });
+
+        it('allows clients to message on another', function(done){
+            client.on('data', function(data){
+                console.log('data are ', data);
+                assert('Test hello' === data);
+                chatRoom.remove(client); 
+                chatRoom.remove(client2);
+                console.log('semifinal clients are ', chatRoom.clients.length);
+                done();
+            });
+            client2.write('Test hello');
+        });
+    
+        // it('removes clients', function(done){
+        //     console.log('final clients are ', chatRoom.clients.length);
+        //     assert(0 === chatRoom.clients.length);
+        //     done();
+        // });
+
+    
+        // after(done => {
+        //     client.once('close', done);
+        //     client.end(done);
+        // });  
+
+        // after(done => {
+        //     server.close(done);
+        // });
+
     });
+     done;
 });
