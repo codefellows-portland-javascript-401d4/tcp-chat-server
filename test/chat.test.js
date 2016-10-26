7b5261259695898d2bd2803e6e00f938bf350f69
nsmeds <niilosmeds@gmail.com>
2016-10-25 17:39:10 -0700

    added semicolon to appease travis

--- test/chat.test.js
+++ test/chat.test.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 const net = require('net');
-const server = require('../index')
+const server = require('../index');
 
 describe('chat server', () => {
 
