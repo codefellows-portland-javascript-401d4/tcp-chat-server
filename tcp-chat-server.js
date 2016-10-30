75d2e433948852f22fcf3bb31226c5b463e91291
caraldi <araldicm@Caitlin-M-Araldi.local>
2016-10-30 16:08:21 -0700

    Added dog ascii to client.name

--- tcp-chat-server.js
+++ tcp-chat-server.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const net = require('net')
-const dogs = require('dog-ascii-faces');
 const DogChat = require('./lib/DogChat');
 const dogChat = new DogChat();
 
@@ -9,11 +8,10 @@ const server = net.createServer(client => {
     client.setEncoding('utf-8');
 
     dogChat.add(client);
-    client.write('Welcome to DogChat', client);
+    client.write('Welcome to DogChat! \n', client);
 
     client.on('data', message => {
         dogChat.send(client, message);
-        console.log(dogs());
     });
 
     client.on('close', () => {
@@ -24,7 +22,7 @@ const server = net.createServer(client => {
 const port = 65000;
 server.listen(port, err => {
     if(err) console.log(`ERROR: ${err}`);
-    else console.log(`server is listening on port: ${port}`);
+    else console.log(`Server is listening on port: ${port}`);
 });
 
 module.exports = server;
