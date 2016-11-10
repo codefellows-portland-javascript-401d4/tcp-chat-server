61056e52101dfac485a90e3e45e0a0216f31385d
Gregory Katchmar <gunk55@msn.com>
2016-11-09 22:54:53 -0800

    1109-2255 ready for test

--- chat.js
+++ chat.js
@@ -1,37 +1,25 @@
-"use strict";
-
-const net = require("net");
-var generateName = require("sillyname");
-
-let i = 1;
-const clients = [];
+const net = require('net');
+const Chatter = require('./Chatters');
+const chatter = new Chatter();
 
 const server = net.createServer(client => {
-  const name = "client " + (i++);
- 	const nickname = generateName();
-  client.setEncoding("utf-8");
-  console.log(`${nickname} (${name}) has connected`);
+  client.setEncoding('utf-8');
 
-  clients.push(client);
-  // console.log(clients);
+  chatter.add(client);
+  console.log(client.name, 'has connected');
 
-  client.on("data", message => {
-    clients.forEach(c => {
-      if(c === client) return; //return to .forEach "loop"
-      
-        c.write(nickname + ": " + client.message);
-                });
+  client.on('data', message => {
+    chatter.send(client, message);
   });
 
-  client.on("close", () => {
-    const index = clients.indexOf(client);
-    if (index !== -1) clients.splice(index, 1);
-    console.log(`${nickname} (${name}) has disconnected`);
+  client.on('close', () => {
+    chatter.remove(client);
+    console.log(client.name, 'has disconnected');
   });
 });
 
 const port = 65000;
 server.listen(port, err => {
-  if (err) console.log("ERROR!", err);
-  else console.log("server listening on port", port);
+  if (err) console.log('ERROR!', err);
+  else console.log('server listening on port', port);
 });
